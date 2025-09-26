9e839a1e77e7648dbdf74fd6a643bcfee39d91cc
Kalyan <kalyanaramana_123@yahoo.co.in>
2025-09-26 11:56:27 +0530

    Fix vercel.json (UTF-8) and serverless entry

--- api/index.py
+++ api/index.py
@@ -3,6 +3,4 @@ from django.core.wsgi import get_wsgi_application
 
 # Point to your Django settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "store.settings")
-
-# Vercel looks for pp or handler
 app = get_wsgi_application()
